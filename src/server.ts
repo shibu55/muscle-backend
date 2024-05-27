825b6e4762b5c7aedfd1640babe896c7fefb70fe
shibu55 <segusuigao@gmail.com>
2024-05-27 22:59:41 +0900

    cors

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import PingController from './controllers/ping';
+const cors = require('cors');
 
 const app = express();
 const port = 8080;
 
 const pingController = new PingController();
-
+app.use(cors());
 app.get('/ping', (req, res) => pingController.ping(req, res));
 
 app.listen(port, () => {
